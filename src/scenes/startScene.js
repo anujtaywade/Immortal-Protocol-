1754e7314712d161069b22a1240104a824dd1dcd
anuj taywade <anujtaywade456@gmail.com>
2025-12-03 19:45:54 +0530

    font

--- src/scenes/startScene.js
+++ src/scenes/startScene.js
@@ -1,47 +1,40 @@
-
-
 export class startScene extends Phaser.Scene {
-    constructor() {
-        super('startScene');
-    }
+  constructor() {
+    super("startScene");
+  }
 
-    
+  preload() {
+    this.load.image("background", "assets/background/mainBackground.png");
+  }
 
+  create() {
+    this.background = this.add.tileSprite(640, 360, 1280, 460, "background");
+    this.titletext = this.add.text(450, 50, "Immortal protocol", {
+      font: "40px",
+      fontFamily : "Press Start 2P",
+      fill: "#00ff00",
+    });
+    this.tweens.add({
+      targets: this.titletext,
+      x: this.titletext.x + 10,
+      duration: 580,
+      yoyo: true,
+      repeat: -1,
+    });
 
-    preload() {
-        this.load.image("background","assets/background/mainBackground.png")
-        
-    }
+    var animationConfig = {
+      key: "shake",
+      frames: this.anims.generateFrameNumbers("Logoname", {
+        start: 0,
+        end: 5,
+      }),
+      repeat: -1,
+      frameRate: 10,
+    };
+    this.anims.create(animationConfig);
+  }
 
-    
-    create() {
-        this.background = this.add.tileSprite(640, 360, 1280, 460,"background")
-        this.titletext = this.add.text(450,50,"Immortal protocol",{font : "40px courier",fill : "#00ff00"})
-        this.tweens.add({
-            targets : this.titletext,
-            x : this.titletext.x+10,
-            duration : 580,
-            yoyo : true,
-            repeat : -1
-        })
-        
-        var animationConfig = {
-    key : "shake",
-    frames : this.anims.generateFrameNumbers("Logoname",{
-        start : 0,
-        end : 5
-    }),
-    repeat : -1,
-    frameRate : 10
-    
-}
-        this.anims.create(animationConfig)
-    }
-
-   
-
-    update(){
-        this.background.tilePositionX += 0.2
-    }
-
+  update() {
+    this.background.tilePositionX += 0.2;
+  }
 }
