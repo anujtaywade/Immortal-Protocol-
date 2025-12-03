a34d3eb8bc5a7e735f7155f07241759228229695
anuj taywade <anujtaywade456@gmail.com>
2025-12-03 19:39:54 +0530

    animation to title

--- src/scenes/startScene.js
+++ src/scenes/startScene.js
@@ -1,19 +1,44 @@
-// "Every great game begins with a single scene. Let's make this one unforgettable!"
+
 
 export class startScene extends Phaser.Scene {
     constructor() {
         super('startScene');
     }
 
+    
+
 
     preload() {
         this.load.image("background","assets/background/mainBackground.png")
+        
     }
 
     
     create() {
         this.background = this.add.tileSprite(640, 360, 1280, 460,"background")
+        this.titletext = this.add.text(450,50,"Immortal protocol",{font : "40px courier",fill : "#00ff00"})
+        this.tweens.add({
+            targets : this.titletext,
+            x : this.titletext.x+10,
+            duration : 580,
+            yoyo : true,
+            repeat : -1
+        })
+        
+        var animationConfig = {
+    key : "shake",
+    frames : this.anims.generateFrameNumbers("Logoname",{
+        start : 0,
+        end : 5
+    }),
+    repeat : -1,
+    frameRate : 10
+    
+}
+        this.anims.create(animationConfig)
     }
+
+   
 
     update(){
         this.background.tilePositionX += 0.2
