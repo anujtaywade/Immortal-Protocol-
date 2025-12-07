5447c14b67ed84bc19bedcfc5417de9329bb7ccc
anuj taywade <anujtaywade456@gmail.com>
2025-12-08 01:04:13 +0530

    hover event in button

--- src/scenes/startScene.js
+++ src/scenes/startScene.js
@@ -48,15 +48,30 @@ export class startScene extends Phaser.Scene {
       
     })
 
-   this.playBtn = this.add.image(330,300,"playBtn").setInteractive()
-   this.playBtn.setScale(0.8)
+   this.playBtn = this.add.image(330,300,"playBtn").setInteractive({useHandCursor : true}).setScale(0.8)
    
+   this.playBtn.on("pointerover",()=>{
+    this.playBtn.setScale(0.8)
+   })
+
+   this.playBtn.on("pointerout",()=>{
+    this.playBtn.setScale(1)
+   })
+   
+   this.playBtn.on("pointerdown",()=>{
+    this.playBtn.setScale(0.9)
+   })
+
+   this.playBtn.on("pointerup",()=>{
+    console.log("button clicked ")
+   })
 
    this.creditBtn = this.add.image(360,400,"creditBtn").setInteractive()
    this.creditBtn.setScale(1)
 
    this.exitBtn = this.add.image(335,500,"exitBtn").setInteractive()
    this.exitBtn.setScale(0.9)
+   
   }
 
   update() {
